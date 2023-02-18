ec9b9843466895e59d82efa6622b68e939bc467a
Amr Koritem <amr.koritem92@gmail.com>
2023-02-18 03:25:55 +0200

    Initial Commit

---
+++ AKApiManager/AKApiManager.h
@@ -0,0 +1,18 @@
+//
+//  AKApiManager.h
+//  AKApiManager
+//
+//  Created by Amr Koritem on 18/02/2023.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for AKApiManager.
+FOUNDATION_EXPORT double AKApiManagerVersionNumber;
+
+//! Project version string for AKApiManager.
+FOUNDATION_EXPORT const unsigned char AKApiManagerVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <AKApiManager/PublicHeader.h>
+
+
